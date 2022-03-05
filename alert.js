e59ef46647882e47022ec3e85d2d6e0b81b1273a
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-03-05 18:39:07 +0600

    second commit

---
+++ alert.js
@@ -0,0 +1,8 @@
+const name = 15;
+
+const student = {name:'ringky',friend:'sourav',job:'web Developement',
+  makeHome:function () {
+      console.log('name');
+  }
+};
+document.getElementById('name');
